f5d1ff15c25eec8b9293a70004005210ce8db81b
calvickauer <calvickauer@gmail.com>
2023-04-03 18:06:17 -0700

    main features plus grid layout

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 const Welcome = () => {
     return (
+        <div>
         <div className='Home__page'>
             <h1>WRS Garage / Home / Spa Rental</h1>
+        </div>
+        <div className='Main__features'>
+        <div className='Shop__container'>
+            <h1>The Shop</h1>
+            <div className='Shop__home'>
+
+            </div>
+        </div>
+        <div className='Home__container'>
+            <h1>The Home</h1>
+            <div className='Home__home'>
+
+            </div>
+        </div>
+        <div className='Spa__container'>
+            <h1>The Spa</h1>
+            <div className='Spa__home'>
+
+            </div>
+        </div>
+        </div>
         </div>
     )
 }
